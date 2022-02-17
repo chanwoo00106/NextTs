00aa5e104352058d627a6057a251cc33d84db020
변찬우 <chan00106@naver.com>
2022-02-17 11:27:58 +0900

    Add app.ts

--- chatting/server/src/app.ts
+++ chatting/server/src/app.ts
@@ -1,1 +1,27 @@
-console.log("hello");
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import config from "config";
+import log from "../utils/logger";
+
+const port = config.get<number>("port");
+const host = config.get<string>("host");
+const corsOrigin = config.get<string>("corsOrigin");
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: corsOrigin,
+    credentials: true,
+  },
+});
+
+app.get("/", (req, res) => res.send("hello!"));
+
+httpServer.listen(port, host, () => {
+  log.info("🏃 Server is listening 🏃");
+});
