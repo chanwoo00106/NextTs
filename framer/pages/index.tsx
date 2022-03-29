b4dd5f7c2fc27c6220648df93fdbd35a1ea65fe3
변찬우 <chan00106@naver.com>
2022-03-29 20:00:50 +0900

    :busts_in_silhouette: Update index.tsx

--- framer/pages/index.tsx
+++ framer/pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { motion } from "framer-motion";
 
 const Home: NextPage = () => {
   return (
@@ -13,14 +14,22 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <motion.h1
+          animate={{ opacity: 1, top: 0 }}
+          transition={{ duration: 0.8 }}
+          className={styles.title}
+        >
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </motion.h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
+        <motion.p
+          animate={{ opacity: 1, top: 0 }}
+          transition={{ duration: 0.8 }}
+          className={styles.description}
+        >
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        </motion.p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -59,14 +68,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
