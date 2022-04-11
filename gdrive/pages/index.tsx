8be931f8c68bbd250d6020dc29406a77aa580c5b
변찬우 <chan00106@naver.com>
2022-04-11 13:33:25 +0900

    Add 대충 Ui 구성

--- gdrive/pages/index.tsx
+++ gdrive/pages/index.tsx
@@ -1,9 +1,39 @@
-import { Box, useColorMode } from "@chakra-ui/react";
+import { Box, Button, Center } from "@chakra-ui/react";
+import { AiOutlineUser } from "react-icons/ai";
 import type { NextPage } from "next";
+import { useRef } from "react";
 
 const Home: NextPage = () => {
-  const { toggleColorMode } = useColorMode();
-  return <Box height="100vh" onClick={toggleColorMode}></Box>;
+  const FileRef = useRef<HTMLInputElement>(null);
+  return (
+    <>
+      <Box
+        height="3rem"
+        position="fixed"
+        top={0}
+        left={0}
+        width="100%"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        padding="0 3rem"
+        gap="2rem"
+      >
+        <Box fontSize="2rem">GDRIVE</Box>
+        <AiOutlineUser size="2rem" />
+      </Box>
+      <Center display="flex" alignItems="center" height="100vh">
+        <input type="file" ref={FileRef} style={{ display: "none" }} />
+        <Button
+          size="lg"
+          onClick={() => FileRef.current?.click()}
+          colorScheme="blue"
+        >
+          업로드 하기
+        </Button>
+      </Center>
+    </>
+  );
 };
 
 export default Home;
