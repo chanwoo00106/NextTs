6cd01c92727e4a32e55a06ed570b7df9c8b8e374
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-05-01 13:46:33 +0900

    Add responsive

--- gdrive/src/components/Header/index.tsx
+++ gdrive/src/components/Header/index.tsx
@@ -5,50 +5,98 @@ import {
   Switch,
   useColorMode,
   useColorModeValue,
+  useDisclosure,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import React from "react";
 import { AiOutlineUser } from "react-icons/ai";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 export default function MainPage() {
   const background = useColorModeValue("gray.100", "gray.800");
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { register, handleSubmit } = useForm();
   const router = useRouter();
-  const onSubmit = ({ search }: any) => router.push(`/file/${search}`);
   const { toggleColorMode } = useColorMode();
 
+  const onSubmit = ({ search }: any) => {
+    router.push(`/file/${search}`);
+    onClose();
+  };
+
+  const handleToggle = () => (isOpen ? onClose() : onOpen());
+
   return (
-    <Box
-      height="4rem"
-      position="fixed"
-      top={0}
-      left={0}
-      width="100%"
-      display="flex"
-      justifyContent="space-between"
-      alignItems="center"
-      px="5"
-      gap="2rem"
-      background={background}
-    >
-      <Link href="/">
-        <a>
-          <Box fontSize="2rem">GDRIVE</Box>
-        </a>
-      </Link>
-      <Flex gap="1rem">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Input {...register("search")} placeholder="파일 검색" />
-        </form>
-        <Switch colorScheme="gray.200" size="lg" onChange={toggleColorMode} />
-        <Link href="/my">
+    <>
+      <Box
+        zIndex={20}
+        height="4rem"
+        position="fixed"
+        top={0}
+        left={0}
+        width="100%"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        px="5"
+        gap="2rem"
+        background={background}
+      >
+        <Link href="/">
           <a>
-            <AiOutlineUser size="2rem" />
+            <Box fontSize="2rem">GDRIVE</Box>
           </a>
         </Link>
-      </Flex>
-    </Box>
+
+        <Flex gap="1rem" display={{ base: "none", md: "flex" }}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input {...register("search")} placeholder="파일 검색" />
+          </form>
+          <Switch colorScheme="gray.200" size="lg" onChange={toggleColorMode} />
+          <Link href="/my">
+            <a>
+              <AiOutlineUser size="2rem" />
+            </a>
+          </Link>
+        </Flex>
+
+        <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
+          <HamburgerIcon fontSize="2rem" cursor="pointer" />
+        </Box>
+      </Box>
+      <Box
+        position="absolute"
+        w="100%"
+        h="100vh"
+        zIndex={2}
+        background="blackAlpha.300"
+        display={isOpen ? "block" : "none"}
+        onClick={() => handleToggle()}
+      />
+      <Box
+        background={background}
+        position="fixed"
+        mt="4rem"
+        display={isOpen ? "block" : "none"}
+        w="100%"
+        zIndex="20"
+      >
+        <Box py="1rem" px="2rem">
+          <Link href="/my">
+            <a onClick={() => onClose()}>My Page</a>
+          </Link>
+        </Box>
+        <Box py="1rem" px="2rem">
+          <Switch colorScheme="gray.200" size="lg" onChange={toggleColorMode} />
+        </Box>
+        <Box py="1rem" px="2rem">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input {...register("search")} placeholder="파일 검색" />
+          </form>
+        </Box>
+      </Box>
+    </>
   );
 }
