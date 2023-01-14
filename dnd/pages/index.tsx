2840fa648cbde810fcd0256139bc2d4ef78e6d61
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-01-14 23:52:47 +0900

    Restart && Add dependencies

--- dnd/pages/index.tsx
+++ dnd/pages/index.tsx
@@ -1,29 +1,3 @@
-import { useState } from "react";
-import Board from "../components/Board";
-import Card from "../components/Card";
-
-const list = [...Array(5)].map((_, id) => ({
-  id,
-  title: `Number ${id}`,
-  description: `My name is number ${id}`,
-}));
-
 export default function Home() {
-  const [cards, setCards] = useState(list);
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        gap: "1rem",
-      }}
-    >
-      <Board>
-        {cards.map((card) => (
-          <Card key={card.id} card={card} />
-        ))}
-      </Board>
-      <Board></Board>
-    </div>
-  );
+  return <div>hello world</div>;
 }
