0f97d2116f58e23f2d349ba6f8630cf4e07f8184
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-01-15 14:28:46 +0900

    Add theme config

--- dnd/pages/_app.tsx
+++ dnd/pages/_app.tsx
@@ -2,11 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../config/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <Component {...pageProps} />
-    </DndProvider>
+    <ChakraProvider theme={theme}>
+      <DndProvider backend={HTML5Backend}>
+        <Component {...pageProps} />
+      </DndProvider>
+    </ChakraProvider>
   );
 }
