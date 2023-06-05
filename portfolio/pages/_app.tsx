b5ae195bc56b0621fc63ad7de3acb39e2b8cde90
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-06-05 19:34:45 +0900

    feat: add font setting

--- portfolio/pages/_app.tsx
+++ portfolio/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
+import { Montserrat } from 'next/font/google'
 import type { AppProps } from 'next/app'
 
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-mont',
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${montserrat.variable} font-mont`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -11,6 +13,9 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        mont: ['var(--font-mont)', ...fontFamily.sans],
       },
     },
   },
