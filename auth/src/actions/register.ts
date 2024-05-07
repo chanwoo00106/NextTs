ab3ddc16d0ed27bd1470a262f20572c3f387b6fe
변찬우 <byun00106@gmail.com>
2024-05-07 11:27:09 +0900

    feat: add register logic

--- auth/src/actions/register.ts
+++ auth/src/actions/register.ts
@@ -2,6 +2,9 @@
 
 import { RegisterSchema } from "@/schemas";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +13,22 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "User Created!" };
 };
