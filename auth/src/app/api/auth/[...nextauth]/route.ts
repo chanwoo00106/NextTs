39ae4b833a00e4b49d3b267812ada833185efcf7
변찬우 <byun00106@gmail.com>
2024-05-09 21:43:36 +0900

    feat: add callback

--- auth/src/app/api/auth/[...nextauth]/route.ts
+++ auth/src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,25 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
+import { db } from "@/lib/db";
 
 export const {
   handlers: { GET, POST },
-} = NextAuth({ ...authConfig });
+} = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+
+    async jwt({ token }) {
+      return token;
+    },
+  },
+
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
